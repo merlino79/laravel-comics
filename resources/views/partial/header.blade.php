91752e583afae5f9839cadee84753ef17c60f1f9
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-22 15:22:43 +0200

    probemi link

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -6,6 +6,22 @@
           <img src="{{ asset('img/dc-logo.png') }}" alt="Logo">
        </div>
      </div>
+     <div class="col">
+       <div class="link my-4">
+         <ul class="">
+           <li class=""><a href="">characters</a></li>
+           <li><a href="">comics</a></li>
+           <li><a href="">movies</a></li>
+           <li><a href="">tv</a></li>
+           <li><a href="">games</a></li>
+           <li><a href="">collectibles</a></li>
+           <li><a href="">videos</a></li>
+           <li><a href="">fans</a></li>
+           <li><a href="">news</a></li>
+           <li><a href="">shop</a></li>
+         </ul>
+       </div>
+     </div>
    </div>
  </div>
 </header>
