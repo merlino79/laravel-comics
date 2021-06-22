5af54bd2f0a7df63699577b549722a73a559971e
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-22 22:50:00 +0200

    ciclo foreach sui primi due film

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -7,8 +7,12 @@
 
         <div class="row">
           <div class="col">
-            <div class="img">
-              <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+            @foreach ($films as $film )
+              <div class="img">
+                <img src="{{$film['thumb']}}" alt="">
+              </div>
+              
+            @endforeach
             </div>
 
            
--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,22 @@ Route::get('/characters', function () {
 })->name('characters');
 
 Route::get('/movies', function () {
-    return view('movies');
+    $films = [
+    [
+      "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+      "price" => "$19.99",
+      "series" => "Action Comics",
+      "type" => "comic book"
+    ],
+    [
+      "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+      "price" => "$3.99",
+        "series"  => "American Vampire 1976",
+        "type"  => "comic book",
+    ],
+    ];
+    return view('movies', ['films'=>$films]);
 })->name('movies');
 
-// Route::get('/films', function () {
-//   // i dati messi in config sono centralizzati per tutto il progetto
-//   $pasta = config('pasta');
-//   return view('prodotti', ['products' => $pasta]);
-// })->name('prodotti');
+
 
