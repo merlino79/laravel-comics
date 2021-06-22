fdb3b086157f805ec91a3b5a07280266589a12a3
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-22 14:55:26 +0200

    inserimento logo

---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+ <div class="container">
+   <div class="row">
+     <div class="col">
+       <div class="img-logo my-2">
+          <img src="{{ asset('img/dc-logo.png') }}" alt="Logo">
+       </div>
+     </div>
+   </div>
+ </div>
+</header>
