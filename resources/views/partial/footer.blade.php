ea361f464167efec2cb8c91d913490aae606209a
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-22 17:56:07 +0200

    errore riga 25

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,3 +1,71 @@
 <footer>
-  footer
+  <div class="container">
+    <div class="row">
+      <div class="col">
+         <div class="btn">
+      <button class="bottone">
+        <a href="#">
+     
+            SIGN-UP NOW!
+   
+        </a>
+      </button>
+
+    </div>
+    <div class="follow">
+      <ul>
+        <li>
+      
+           <a href="#">FOLLOW US</a>
+     
+      </li>
+       <li>
+      
+           <a href="#">
+             <img src="{{ asset(img/footer-facebook.png) }}" alt="facebook">
+           </a>
+     
+      </li>
+       <li>
+      
+           <a href="#">
+             <img src="{{ asset(img/footer-twitter.png) }}" alt="twitter">
+           </a>
+     
+      </li>
+       <li>
+      
+           <a href="#">
+              <img src="{{ asset(img/footer-twitter.png)}}" alt="youtube">
+           </a>
+     
+      </li>
+       <li>
+      
+           <a href="#">
+             <img src="{{ asset(img/footer-youtube.png)}}" alt="pinterest">
+            
+           </a>
+     
+      </li>
+       <li>
+      
+           <a href="#">
+             <img src="{{ asset(img/footer-pinterest.png)}}" alt="periscope">
+           </a>
+     
+      </li>
+      </ul>
+     
+     
+      
+
+    </div>
+  </div>
+ 
+
+</div>
+      </div>
+    </div>
+  </div>
 </footer>
